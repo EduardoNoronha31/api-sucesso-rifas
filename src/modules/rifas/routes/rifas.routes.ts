86e7b02a88decd4ff6c672d7757bb1e95dd165a2
Eduardo Noronha <eduardonoronha3101@gmail.com>
2022-04-05 11:14:16 -0300

    feat: celebrate added to validate the request data

--- src/modules/rifas/routes/rifas.routes.ts
+++ src/modules/rifas/routes/rifas.routes.ts
@@ -1,13 +1,57 @@
 import { Router } from 'express';
 import RifasController from '../controllers/RifasController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const rifasRouter = Router();
 const rifasController = new RifasController();
 
 rifasRouter.get('/', rifasController.index);
-rifasRouter.get('/:id', rifasController.show);
-rifasRouter.post('/', rifasController.create);
-rifasRouter.put('/:id', rifasController.update);
-rifasRouter.delete('/:id', rifasController.delete);
+
+rifasRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  rifasController.show,
+);
+
+rifasRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  rifasController.create,
+);
+
+rifasRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  rifasController.update,
+);
+
+rifasRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  rifasController.delete,
+);
 
 export default rifasRouter;
